a95bf90877a5d9f4f0548aa410322e000551be73
sultan <aidaraliuulu@gmail.com>
2019-03-23 23:45:18 +0600

    Показываю все контакты на странице. Создал модалку для показа информации о контакта и написал стили

--- src/containers/MainContainer/MainContainer.js
+++ src/containers/MainContainer/MainContainer.js
@@ -1,19 +1,88 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {fetchContact} from "../../store/actions/contactSyncActions";
 import {connect} from "react-redux";
+import './MainContainer.css';
+import {Button, Card, CardBody, CardTitle, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
 
 class MainContainer extends Component {
+
+    state = {
+        selectedContact: null
+    };
 
     componentDidMount() {
         this.props.fetchContacts()
     }
 
-    render() {
-        console.log(this.props.contacts);
-        return (
-            <div>
+    showModal = contact => {
+        this.setState({selectedContact: contact});
+    };
 
-            </div>
+    hideModal = () => {
+        this.setState({selectedContact: null});
+    };
+
+    EditHandler = (id, event) => {
+        event.preventDefault();
+        this.props.history.push(`/edit/:${id}`)
+    };
+
+    render() {
+        const contacts = this.props.contacts.map((contact) => {
+            return (
+                <div onClick={() => this.showModal(contact)} key={contact.id}>
+                    <Card className=" ContactItemBlock">
+                        <CardBody className="ContactItemBlockInfo">
+                            <CardTitle>{contact.name}</CardTitle>
+                            <CardTitle>{contact.username}</CardTitle>
+                        </CardBody>
+                    </Card>
+                </div>
+            )
+        });
+        return (
+            <Fragment>
+                <div>
+                    <h2 style={{margin: "5px"}}>Contacts</h2>
+                </div>
+                <div className="Contacts">
+                    {contacts}
+                </div>
+
+                <Modal isOpen={!!this.state.selectedContact} toggle={this.hideModal}>
+                    {this.state.selectedContact && (
+                        <Fragment>
+                            <ModalHeader toggle={this.hideModal}>{this.state.selectedContact.name}</ModalHeader>
+                            <ModalBody >
+                                <div className="modalBody">
+                                    <div>
+                                        <img className="Contact-avatar"
+                                             src={this.state.selectedContact.avatar} alt="avatar"/>
+                                    </div>
+                                    <div className="info">
+                                        <p>User name: <strong>{this.state.selectedContact.username}</strong></p>
+                                        <p>Phone: <strong>{this.state.selectedContact.phone}</strong></p>
+                                        <div>Email: <a href="/"> {this.state.selectedContact.email}</a></div>
+                                        <div>Website: <a href="/"> {this.state.selectedContact.website}</a></div>
+                                        <div style={{marginTop: '10px'}}>
+                                            <h4 style={{fontWeight: 'bold'}}>Address</h4>
+                                            <div>
+                                                <p>Country: <strong>{this.state.selectedContact.address.country}</strong></p>
+                                                <p>City: <strong>{this.state.selectedContact.address.city}</strong></p>
+                                                <p>State: <strong>{this.state.selectedContact.address.state}</strong></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="primary" onClick={this.EditHandler.bind(this, this.state.selectedContact.id)}>Edit</Button>{' '}
+                                <Button color="danger" onClick={this.hideModal}>Cancel</Button>
+                            </ModalFooter>
+                        </Fragment>
+                    )}
+                </Modal>
+            </Fragment>
         );
     }
 }
